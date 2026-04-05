14427dffd726484f803d9c478543db4ac2d910ab
moM3aty <mo.m3aty@gmail.com>
2026-04-05 17:41:52 +0200

    SadStore

--- SadStore/obj/Debug/net8.0/SadStore.AssemblyInfo.cs
+++ SadStore/obj/Debug/net8.0/SadStore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SadStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d03152f4a8adc6b1fcb019cdb24e5a865ea59e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+780e39481a463ba27adcbe8cc39805169683c5c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SadStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SadStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
